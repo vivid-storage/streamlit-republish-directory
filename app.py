bbe84e3366761379c99aa4ba38d740a10dd093d7
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2024-04-25 10:43:00 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("""
+# My first app
+Hello *everybody!* 
+
+This is coming to you live from the GH root!
+
+Thanks for using Posit Connect Cloud!
+""")
---
+++ republished-directory/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("""
+# My first app
+Hello *everybody!*
+
+Republished from a nested directory!
+
+Thanks for using Posit Connect Cloud!
+""")
